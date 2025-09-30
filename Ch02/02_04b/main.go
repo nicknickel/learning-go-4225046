0cfb3ce11a1cc58640c41fb627abef92806a8d86
Zach Nickel <zachnickel@gmail.com>
2025-09-30 21:00:02 +0000

    More learning

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
+
+	total := float64(i1) + f2
+	fmt.Println("Total sum:", total)
+
+	product := float64(i1) * f2
+	fmt.Println("Product sum:", product)
+
+	quotient := float64(i2) / f3
+	fmt.Println("Quotient sum:", quotient)
 }
