7bf181a3de846716106f6ca9ae58a1abd70cff03
Zach Nickel <zachnickel@gmail.com>
2025-10-02 19:10:07 +0000

    next chapter

--- Ch04/04_01b/main.go
+++ Ch04/04_01b/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	theAnswer := 42
+	var result string
+
+	if theAnswer < 0 {
+		result = "less than zero"
+	} else if theAnswer == 0 {
+		result = "equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+
+	fmt.Println(result)
+	fmt.Printf("value of answer %v\n", theAnswer)
 }
--- Ch04/04_03b/main.go
+++ Ch04/04_03b/main.go
@@ -41,7 +41,9 @@ func main() {
 			goto theEnd
 		}
 	}
-	theEnd : println("end of program")
+
+theEnd:
+	println("ENd of program")
 	fmt.Printf("Sum: %v\n", sum)
 
 }
